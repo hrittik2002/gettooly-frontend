dc8c9028ac1b0d8c441aec88760a89c9e04f8191
Hrittik <hrittikgarain@gmail.com>
2023-04-13 00:14:15 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { ChakraProvider } from '@chakra-ui/react';
-import { store } from './redux/store';
-import { Provider } from'react-redux';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { ChakraProvider } from "@chakra-ui/react";
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
+import { persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <ChakraProvider>
-    <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </ChakraProvider>
   </Provider>
 );
-
--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -2,11 +2,26 @@ import { configureStore } from '@reduxjs/toolkit'
 import userReducer from './userSlice'
 import formReducer from './formSlice'
 import questionsReducer from './questionsSlice'
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
+import thunk from 'redux-thunk';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedUserReducer = persistReducer(persistConfig, userReducer)
+const persistedFormReducer = persistReducer(persistConfig, formReducer)
+const persistQuestionsReducer = persistReducer(persistConfig, questionsReducer)
 
 export const store = configureStore({
   reducer: {
-    user : userReducer,
-    form : formReducer,
-    questions : questionsReducer,
+    user : persistedUserReducer,
+    form : persistedFormReducer,
+    questions : persistQuestionsReducer,
   },
 })
+
+export const persistor = persistStore(store)
+
