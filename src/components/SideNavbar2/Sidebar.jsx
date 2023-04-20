c1cb0cf1f79ab529cacbe56b85db386ae7c828fb
Hrittik <hrittikgarain@gmail.com>
2023-04-20 12:33:14 +0530

    updated

--- src/components/SideNavbar2/Sidebar.jsx
+++ src/components/SideNavbar2/Sidebar.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 import styles from "./styles.module.css";
 import HomeIcon from "@mui/icons-material/Home";
+import PersonIcon from "@mui/icons-material/Person";
+import LockOpenIcon from "@mui/icons-material/LockOpen";
+import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
+import ArticleIcon from "@mui/icons-material/Article";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 const Sidebar = ({ sidebar }) => {
+  const navigate = useNavigate();
+  const userData = useSelector((state) => state.user.currentUser)
+  const changePage = (link) => {
+    navigate(link)
+  }
   return (
     <div
       className={
@@ -10,14 +21,37 @@ const Sidebar = ({ sidebar }) => {
           : `${styles.sidebar}`
       }
     >
-      <li>
+      <li className={styles.listItem}
+        onClick={()=>{changePage('/')}}
+      >
         <HomeIcon />
         Home
       </li>
-      <li>Products</li>
-      <li>Carrer</li>
-      <li>Contact Us</li>
-      <li>About Us</li>
+      <li className={styles.listItem}
+      onClick={()=>{changePage(`/ConductUser/${userData.id}`)}}
+      > 
+        <ArticleIcon />
+        CreateForm
+      </li>
+      <li className={styles.listItem}
+      onClick={()=>{changePage(`/ConductUser/${userData.id}/about`)}}
+      >
+        <PersonIcon />
+        About
+      </li>
+      <li className={styles.listItem}
+      onClick={()=>{changePage( `/ConductUser/${userData.id}/changePassword`)}}
+      >
+        <LockOpenIcon />
+        Change Password
+      </li>
+      <li className={styles.listItem} 
+      onClick={()=>{changePage(`/ConductUser/${userData.id}/subscription`)}}
+      >
+        <SubscriptionsIcon />
+
+        Subscriptions
+      </li>
     </div>
   );
 };
