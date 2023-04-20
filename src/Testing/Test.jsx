bfa7b8406b922be3c5836d183129168b31700a87
Hrittik <hrittikgarain@gmail.com>
2023-04-20 19:42:41 +0530

    updated

--- src/Testing/Test.jsx
+++ src/Testing/Test.jsx
@@ -1,13 +1,68 @@
-import React from 'react'
-import SideNavbar2 from '../components/SideNavbar2/SideNavbar2'
-
+import React from "react";
+import SideNavbar2 from "../components/SideNavbar2/SideNavbar2";
+import styles from "./Test.module.css";
 const Test = () => {
   return (
     <>
-    <SideNavbar2/>
-    
+      <SideNavbar2 />
+      <div className={styles.saasSubscriptionCard}>
+        <div className={styles.saasSubscriptionCardHeader}>
+          <h2>Get Started</h2>
+          <p>Choose a plan that works for you</p>
+        </div>
+        <div className={styles.saasSubscriptionCardBody}>
+        <div className={styles.saasSubscriptionCardPlan}>
+            <h3>Pro</h3>
+            <p className={styles.saasSubscriptionCardPrice}>$49/month</p>
+            <ul className={styles.saasSubscriptionCardFeatures}>
+              <li>5 Users</li>
+              <li>20 GB Storage</li>
+              <li>Email and Phone Support</li>
+            </ul>
+            <button className={styles.saasSubscriptionCardCta}>
+              Choose Plan
+            </button>
+          </div>
+          <div className={styles.saasSubscriptionCardPlan}>
+            <h3>Pro</h3>
+            <p className={styles.saasSubscriptionCardPrice}>$49/month</p>
+            <ul className={styles.saasSubscriptionCardFeatures}>
+              <li>5 Users</li>
+              <li>20 GB Storage</li>
+              <li>Email and Phone Support</li>
+            </ul>
+            <button className={styles.saasSubscriptionCardCta}>
+              Choose Plan
+            </button>
+          </div>
+          <div className={styles.saasSubscriptionCardPlan}>
+            <h3>Enterprise</h3>
+            <p className={styles.saasSubscriptionCardPrice}>$99/month</p>
+            <ul className={styles.saasSubscriptionCardFeatures}>
+              <li>Unlimited Users</li>
+              <li>50 GB Storage</li>
+              <li>Email, Phone, and Chat Support</li>
+            </ul>
+            <button className={styles.saasSubscriptionCardCta}>
+              Choose Plan
+            </button>
+          </div>
+          <div className={styles.saasSubscriptionCardPlan}>
+            <h3>Enterprise</h3>
+            <p className={styles.saasSubscriptionCardPrice}>$99/month</p>
+            <ul className={styles.saasSubscriptionCardFeatures}>
+              <li>Unlimited Users</li>
+              <li>50 GB Storage</li>
+              <li>Email, Phone, and Chat Support</li>
+            </ul>
+            <button className={styles.saasSubscriptionCardCta}>
+              Choose Plan
+            </button>
+          </div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Test
+export default Test;
--- src/pages/SubscriptionPage/SubscriptionPage.jsx
+++ src/pages/SubscriptionPage/SubscriptionPage.jsx
@@ -1,103 +1,77 @@
-import React from 'react'
-import styles from './SubscriptionPage.module.css'
-import SideNavbar from '../../components/SideNavbar/SideNavbar'
-import { Box, Button, Card, CardBody, CardFooter, CardHeader, Heading, SimpleGrid, Text } from '@chakra-ui/react'
-import { useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import SideNavbar2 from '../../components/SideNavbar2/SideNavbar2';
+import React from "react";
+import styles from "./SubscriptionPage.module.css";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import SideNavbar2 from "../../components/SideNavbar2/SideNavbar2";
 
-
-const tiers = [
-    {
-      title: 'Free',
-      price: '0',
-      description: [
-        '10 users included',
-        '2 GB of storage',
-        'Help center access',
-        'Email support',
-      ],
-      buttonText: 'Sign up for free',
-      buttonVariant: 'outlined',
-    },
-    {
-      title: 'Pro',
-      subheader: 'Most popular',
-      price: '15',
-      description: [
-        '20 users included',
-        '10 GB of storage',
-        'Help center access',
-        'Priority email support',
-      ],
-      buttonText: 'Get started',
-      buttonVariant: 'contained',
-    },
-    {
-      title: 'Enterprise',
-      price: '30',
-      description: [
-        '50 users included',
-        '30 GB of storage',
-        'Help center access',
-        'Phone & email support',
-      ],
-      buttonText: 'Contact us',
-      buttonVariant: 'outlined',
-    },
-  ];
 const SubscriptionPage = () => {
-    const navigate = useNavigate();
-    const userData = useSelector((state) => state.user.currentUser);
-    const basicPayHandler = () =>{
-        console.log(userData)
-        navigate(`/ConductUser/${userData.id}/payment`);
-    }
+  const navigate = useNavigate();
+  const userData = useSelector((state) => state.user.currentUser);
+  const basicPayHandler = () => {
+    console.log(userData);
+    navigate(`/ConductUser/${userData.id}/payment`);
+  };
   return (
-    <div >
+    <div>
       <SideNavbar2 />
-      <Box   display="flex" flexDirection="column"  alignItems="center" width="100%"  marginTop="2%">
-        <Heading>Subscription Plans</Heading>
-        <SimpleGrid width="50%" display="flex" justifyContent="center" alignItems="center"  spacing={4} templateColumns='repeat(auto-fill, minmax(200px, 1fr))'>
-  <Card flex="1">
-    <CardHeader>
-      <Heading size='md'> Free</Heading>
-    </CardHeader>
-    <CardBody>
-      <Text>0$</Text>
-    </CardBody>
-    <CardFooter>
-      <Button >Buy Now</Button>
-    </CardFooter>
-  </Card>
-  <Card flex="1">
-    <CardHeader>
-      <Heading size='md'> Basic</Heading>
-    </CardHeader>
-    <CardBody>
-      <Text >$10</Text>
-    </CardBody>
-    <CardFooter>
-      <Button onClick={basicPayHandler}>Buy Now</Button>
-    </CardFooter>
-  </Card>
-  <Card flex="1">
-    <CardHeader>
-      <Heading size='md'>Unlimited</Heading>
-    </CardHeader>
-    <CardBody>
-      <Text>$30</Text>
-    </CardBody>
-    <CardFooter>
-      <Button>Buy Now</Button>
-    </CardFooter>
-  </Card>
-</SimpleGrid>
-            
-        
-      </Box>
+      <div className={styles.saasSubscriptionCard}>
+        <div className={styles.saasSubscriptionCardHeader}>
+          <h2>Get Started</h2>
+          <p>Choose a plan that works for you</p>
+        </div>
+        <div className={styles.saasSubscriptionCardBody}>
+        <div className={styles.saasSubscriptionCardPlan}>
+            <h3>Pro</h3>
+            <p className={styles.saasSubscriptionCardPrice}>$10/month</p>
+            <ul className={styles.saasSubscriptionCardFeatures}>
+              <li>10k Users</li>
+              <li>20 GB Storage</li>
+              <li>Email and Phone Support</li>
+            </ul>
+            <button className={styles.saasSubscriptionCardCta} onClick={basicPayHandler}>
+              Choose Plan
+            </button>
+          </div>
+          <div className={styles.saasSubscriptionCardPlan}>
+            <h3>Pro</h3>
+            <p className={styles.saasSubscriptionCardPrice}>$49/month</p>
+            <ul className={styles.saasSubscriptionCardFeatures}>
+              <li>50k Users</li>
+              <li>20 GB Storage</li>
+              <li>Email and Phone Support</li>
+            </ul>
+            <button className={styles.saasSubscriptionCardCta}>
+              Choose Plan
+            </button>
+          </div>
+          <div className={styles.saasSubscriptionCardPlan}>
+            <h3>Enterprise</h3>
+            <p className={styles.saasSubscriptionCardPrice}>$99/month</p>
+            <ul className={styles.saasSubscriptionCardFeatures}>
+              <li>Unlimited Users</li>
+              <li>150 GB Storage</li>
+              <li>Email, Phone, and Chat Support</li>
+            </ul>
+            <button className={styles.saasSubscriptionCardCta}>
+              Choose Plan
+            </button>
+          </div>
+          <div className={styles.saasSubscriptionCardPlan}>
+            <h3>Enterprise</h3>
+            <p className={styles.saasSubscriptionCardPrice}>$199/month</p>
+            <ul className={styles.saasSubscriptionCardFeatures}>
+              <li>Unlimited Users</li>
+              <li>50 GB Storage</li>
+              <li>Email, Phone, and Chat Support</li>
+            </ul>
+            <button className={styles.saasSubscriptionCardCta}>
+              Choose Plan
+            </button>
+          </div>
+        </div>
       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default SubscriptionPage
+export default SubscriptionPage;
