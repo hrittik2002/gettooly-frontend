4a3277ee6f990d0e9053b08ee7bd31619b283d2e
Hrittik <hrittikgarain@gmail.com>
2023-05-14 18:44:39 +0530

    creatednavbarResponsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,8 +5,18 @@ import Register from "../Register/Register";
 import { Button } from "@chakra-ui/react";
 import Login from "../Login/Login";
 import { Link } from "react-router-dom";
-
-const Navbar = ({location}) => {
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuItemOption,
+  MenuGroup,
+  MenuOptionGroup,
+  MenuDivider,
+} from "@chakra-ui/react";
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+const Navbar = ({ location }) => {
   const [showDialogForRegister, setShowDialogForRegister] = useState(false);
   const [showDialogForLogin, setShowDialogForLogin] = useState(false);
   const closeDialog = () => {
@@ -22,18 +32,38 @@ const Navbar = ({location}) => {
         <div className={styles.logo}>99Types</div>
         <div className={styles.btnContainer}>
           <div className={styles.loginBtnContainer}>
-            <button  className={styles.loginBtn} onClick={() => setShowDialogForLogin(true)}>Login</button>
+            <button
+              className={styles.loginBtn}
+              onClick={() => setShowDialogForLogin(true)}
+            >
+              Login
+            </button>
           </div>
           <div className={styles.registerBtnContainer}>
-            <button className={styles.registerBtn} onClick={() => setShowDialogForRegister(true)}>
+            <button
+              className={styles.registerBtn}
+              onClick={() => setShowDialogForRegister(true)}
+            >
               Register
             </button>
           </div>
         </div>
+        <div className={styles.dropdownContainer}>
+          <Menu isLazy>
+            <MenuButton>
+              <MoreVertIcon  style={{color : "white"}}/>
+            </MenuButton>
+            <MenuList>
+              {/* MenuItems are not rendered unless Menu is open */}
+              <MenuItem onClick={() => setShowDialogForLogin(true)}>Login</MenuItem>
+              <MenuItem onClick={() => setShowDialogForRegister(true)}>Register</MenuItem>
+            </MenuList>
+          </Menu>
+        </div>
       </div>
       {showDialogForRegister && <Register closeDialog={closeDialog} />}
       {showDialogForLogin && (
-        <Login closeDialogForLogin={closeDialogForLogin} location={location}/>
+        <Login closeDialogForLogin={closeDialogForLogin} location={location} />
       )}
     </>
   );
